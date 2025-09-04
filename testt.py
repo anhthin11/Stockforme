86e404dba29498f4e432eb864aec1bab86a585df
anhthin11 <thinhoang352@gmail.com>
2025-09-04 22:08:34 +0700

    Delete testt.py

--- testt.py
+++
@@ -1,8 +0,0 @@
-squares = ['orange', 'orange', 'purple', 'blue ', 'orange']
-new_squares = []
-i = 0
-while(i < len(squares) and squares[i] == 'orange'):
-    new_squares.append(squares[i])
-    i = i + 1
-print (new_squares)
-
